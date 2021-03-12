c0c2c5a30889cda840a111df766b41b32a50ee2a
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-03-12 17:44:06 +0900

    各ファイルの修正と、ヴァリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
+  validates :nickname,        presence: true
+  validates :first_name,      presence: true
+  validates :last_name,       presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana,  presence: true
+  validates :date_of_birth,   presence: true
 end
