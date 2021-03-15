8abf7818f7924da0076c6f3c286558352c4228fd
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-03-15 15:34:37 +0900

    バリデーションの記載とビューファイルの訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :statui_id
+    validates :price, numericality: {
+      only_integer: true,greater_than: 299, less_than: 10000000 
+      }
+    validates :delovery_fee_id
+    validates :delivery_source_id
+    validates :delivery_date_id
+  end
 
+  belongs_to :user
   has_one_attached :image
 end
