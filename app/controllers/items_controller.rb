eca0bc6e357baadd15732ddcd4882f7707c34f4d
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-03-16 18:26:29 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :show, :update, :create]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -23,13 +23,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find{params[:id]}
-    if @item.user.id != current_user.id
-      redirect_to action: :index
-    end
+    check_user
   end
 
   def update
     @item = Item.find{params[:id]}
+    check_user
     if @item.update(item_params)
       redirect_to action: :index
     else
@@ -43,5 +42,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :delivery_fee_id, :delivery_source_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def check_user
+    if @item.user.id != current_user.id
+      redirect_to action: :index
+    end
   end
 end
