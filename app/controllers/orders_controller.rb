b5f57f1c1b5acc14368c61bc4a39b13337521ee9
shincho0922 <79453715+shincho0922@users.noreply.github.com>
2021-03-19 10:38:42 +0900

    オーダーコントローラーの変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,7 +32,7 @@ class OrdersController < ApplicationController
 
   def buy_check
     if @buy.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
         card: buy_params[:token],    # カードトークン
@@ -41,7 +41,6 @@ class OrdersController < ApplicationController
       @buy.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render action: :index
     end
   end
